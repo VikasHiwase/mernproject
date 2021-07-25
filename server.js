e382733d1646a746829f1a82b58005a862c57db2
vikas hiwase <vikas.hiwase20@gmail.com>
2021-07-25 14:07:18 +0530

    added route

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const express = require('express');
 
+let app = express();
+
+app.get('/', (req, res) => {
+    console.log('vikas');
+    res.send('Vikas');
+});
+
+app.get('/api/login', (req, res) => {
+    console.log('vikas');
+    res.send('Login');
+});
+
+const port = process.env.PORT||4000;
+app.listen(port, () => {
+    console.log(`your server is running at ${port}`);
+})
+
+
+// app.listen(4000, () => {
+//     console.log('your server is running at port 4000')
+// })
+
+
 
